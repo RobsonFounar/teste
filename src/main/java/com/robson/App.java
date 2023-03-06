fb83d691c4a2a418187eb0155a38120173c018af
Robson Founar <79342200+RobsonFounar@users.noreply.github.com>
2023-03-06 12:04:58 -0300

    HelloWord VsCode

---
+++ src/main/java/com/robson/App.java
@@ -0,0 +1,13 @@
+package com.robson;
+
+public final class App {
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("IMPRIMIR O QUE EU MANDO");
+    }
+
+}
